0c5af314b380b0a56ce7a663e0d39c698698f95c
darkmana9 <aka.cc@mail.ru>
2021-10-20 11:01:24 +0300

    feat: add lazy loading of pages

--- src/App/App.js
+++ src/App/App.js
@@ -1,29 +1,33 @@
+import React, { lazy, Suspense } from 'react'
 import { Header } from '../components/Header/Header'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme } from '../theme'
 import { LayoutWrapper } from './components'
-import Home from '../pages/Home/Home'
 import Lines from '../components/Lines/Lines'
 import { BrowserRouter, Redirect, Route } from 'react-router-dom'
-import Contacts from '../pages/Contacts/Contacts'
+
+const HomePage = lazy(() => import('../pages/Home/Home'))
+const ContactsPage = lazy(() => import('../pages/Contacts/Contacts'))
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Redirect to='/home'/>
-      <ThemeProvider theme={lightTheme}>
-        <LayoutWrapper>
-          <Lines isTop/>
-          <Header/>
+      <Redirect to="/home"/>
+      <Suspense fallback={<h2>loading...</h2>}>
+        <ThemeProvider theme={lightTheme}>
+          <LayoutWrapper>
+            <Lines isTop/>
+            <Header/>
             <Route path="/home">
-              <Home/>
+              <HomePage/>
             </Route>
             <Route path="/contacts">
-              <Contacts/>
+              <ContactsPage/>
             </Route>
-          <Lines isTop={false}/>
-        </LayoutWrapper>
-      </ThemeProvider>
+            <Lines isTop={false}/>
+          </LayoutWrapper>
+        </ThemeProvider>
+      </Suspense>
     </BrowserRouter>
   )
 }
